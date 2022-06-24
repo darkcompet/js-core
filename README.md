e6db02e877cf553bf522a81e4f35b5baee5ecf72
DarkCompet <darkcompet@gmail.com>
2022-06-24 16:04:16 +0700

    Make npm module with typescript base

---
+++ README.md
@@ -0,0 +1,8 @@
+# Introduction
+
+Module: @darkcompet/js-core
+
+
+## Quick start
+
+todo...
--- constant.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EMPTY_STRING = exports.LINE_FEED = exports.SPACE = void 0;
-exports.SPACE = ' ';
-exports.LINE_FEED = '\n';
-exports.EMPTY_STRING = "";
